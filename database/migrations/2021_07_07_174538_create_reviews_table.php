ff4e124c05175cfaf69ad0c2adb5a9d9a675dcd9
Patrick Röthlisberger <9606082+PatrickRoethlisberger@users.noreply.github.com>
2021-07-07 20:11:04 +0200

    Add Reviews migration

---
+++ database/migrations/2021_07_07_174538_create_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('hotel_id')->constrained();
+            $table->foreignId('reservation_id')->constrained();
+            $table->integer('stars');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
