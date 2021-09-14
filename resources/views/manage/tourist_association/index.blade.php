f142170e74edac1a1f42bd27b19c73ace992c4e2
Patrick Röthlisberger <9606082+PatrickRoethlisberger@users.noreply.github.com>
2021-09-14 10:25:43 +0200

    Add touristAssociation views

---
+++ resources/views/manage/tourist_association/index.blade.php
@@ -0,0 +1,40 @@
+
+<x-app-layout>
+    <x-slot name="header">
+</>
+    </x-slot>
+
+    <x-app-body>
+        <div class="flex items-center justify-between mt-4">
+            <h1 class="font-semibold text-2xl text-gray-800 leading-tight">
+                Verkehrsvereine
+            </h1>
+            <x-link-button href="{{ route('touristAssociation.create') }}">
+                Verkehrsverein erstellen
+            </x-link-button>
+        </div>
+
+        @if ($touristAssociations->count())
+            @foreach ($touristAssociations as $touristAssociation)
+                <x-card>
+                    <x-slot name="title">
+                        {{ $touristAssociation->name }}
+                    </x-slot>
+                    <x-slot name="body">
+                        <p>{{ $touristAssociation->street }}</p>
+                        <p>{{ $touristAssociation->city->PLZ }} {{ $touristAssociation->city->city }}</p>
+                    </x-slot>
+                    <x-slot name="buttons">
+                        <x-link-button :href="route('touristAssociation.edit', [$touristAssociation])">
+                            Bearbeiten
+                        </x-link-button>
+                    </x-slot>
+                </x-card>
+            @endforeach
+            <div class="mt-4">
+                {{ $touristAssociations->links() }}
+            </div>
+        @endif
+
+    </x-app-body>
+</x-app-layout>
