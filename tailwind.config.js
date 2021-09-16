a0d81eac954616c4b5818e12124fdd7d64b82778
Patrick Röthlisberger <9606082+PatrickRoethlisberger@users.noreply.github.com>
2021-09-16 22:27:37 +0200

    Fix tailwind builds missing styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    // mode: 'jit',
-    purge: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+    mode: 'jit',
     presets: [
         // the preset merges, fonts, variants, plugins, colors and purge content
         require('./vendor/tanthammar/tall-forms/resources/stubs/tailwindcss/2.0/tall-forms-preset.js'),
