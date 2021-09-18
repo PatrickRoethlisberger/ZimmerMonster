89f2c646b0d662fd6217b6c2c25afeb260f96cf5
Patrick Röthlisberger <9606082+PatrickRoethlisberger@users.noreply.github.com>
2021-09-18 08:31:31 +0200

    Fix live-view dropdown

--- resources/views/vendor/laravel-views/components/dropdown/drop-down.blade.php
+++ resources/views/vendor/laravel-views/components/dropdown/drop-down.blade.php
@@ -12,8 +12,10 @@ You can customize all the html and css classes but YOU MUST KEEP THE BLADE AND L
 <div
   class="relative"
   x-data="{ open: false }"
+  @click.away="open = false"
+  @close.stop="open = false"
 >
-  <span @click="open = true" class="cursor-pointer">
+  <span @click="open = ! open" class="cursor-pointer">
     @if ($label)
       <x-lv-select-button>
         {{ __($label) }}
@@ -25,10 +27,15 @@ You can customize all the html and css classes but YOU MUST KEEP THE BLADE AND L
     @endif
   </span>
 
-  <div
+  <div x-show="open"
+    x-transition:enter="transition ease-out duration-200"
+    x-transition:enter-start="transform opacity-0 scale-95"
+    x-transition:enter-end="transform opacity-100 scale-100"
+    x-transition:leave="transition ease-in duration-75"
+    x-transition:leave-start="transform opacity-100 scale-100"
+    x-transition:leave-end="transform opacity-0 scale-95"
     class="bg-white shadow-lg rounded absolute top-8 right-0 border text-left z-10 w-{{ isset($dropDownWidth) ? $dropDownWidth : 'full' }}"
     x-show.transition="open"
-    @click.away="open = false"
     x-cloak
   >
     {{ $slot }}
