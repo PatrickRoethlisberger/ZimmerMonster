7f0aa60aed15882e4ba185f516ec00c226a329be
Patrick Röthlisberger <9606082+PatrickRoethlisberger@users.noreply.github.com>
2021-09-19 22:25:53 +0200

    Add api endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\ReviewController;
+use App\Http\Resources\HotelResource;
+use App\Http\Resources\ReviewResource;
+use App\Http\Resources\RoomResource;
+use App\Models\Hotel;
+use App\Models\Review;
+use App\Models\Room;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +20,24 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/hotel', function () {
+    return new HotelResource(Hotel::all());
+});
+
+Route::get('/room', function () {
+    return new RoomResource(Room::all());
+});
+
+Route::get('/review', function () {
+    return new ReviewResource(Review::all());
+});
+
+Route::get('/review/{hotel}', function (Hotel $hotel) {
+    return new ReviewResource(Review::where('hotel_id', $hotel->id)->get());
+});
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
