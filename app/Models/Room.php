c9c9ac5260df8408671e39b8a1748fc11907e9f8
Patrick Röthlisberger <9606082+PatrickRoethlisberger@users.noreply.github.com>
2021-09-19 20:24:34 +0200

    Update room list

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 use Cviebrock\EloquentSluggable\Sluggable;
 use EloquentFilter\Filterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -57,6 +58,31 @@ class Room extends Model
         return $this->hasMany(Reservation::class);
     }
 
+    public function upcommingReservations()
+    {
+        return $this->hasMany(Reservation::class)->where('from', '>=', Carbon::now())->orderBy('from', 'asc');
+    }
+
+    public function currentReservations()
+    {
+        return $this->hasMany(Reservation::class)->where('until', '>=', Carbon::now())->orderBy('from', 'asc');
+    }
+
+    public function getBookedDatesAttribute()
+    {
+        $reservations = $this->hasMany(Reservation::class)->where('until', '>=', Carbon::now())->get();
+
+        $dates = [];
+
+        foreach ($reservations as $reservation) {
+            $startDate = Carbon::parse($reservation->from);
+            $endDate = Carbon::parse($reservation->until);
+            array_push($dates, CarbonPeriod::create($startDate, $endDate)->toArray());
+        }
+
+        return(collect($dates)->flatten());
+    }
+
     public function equipments()
     {
         return $this->morphToMany(Equipment::class, 'equipables', 'equipment_hotel_room')->withPivot('description');
@@ -76,9 +102,5 @@ class Room extends Model
             $query->where('from', '<', $until)
             ->where('until', '>', $from);
         });
-
-
-
-        // return $query->reservations()->where('reservation_date', '>=', $from)->where('reservation_date', '<=', $to)->count() === 0;
     }
 }
--- resources/views/room/index.blade.php
+++ resources/views/room/index.blade.php
@@ -1,8 +1,17 @@
 
 <x-app-layout>
     <x-app-body>
+        <h1 class="font-semibold text-2xl text-gray-800 leading-tight">
+            Zimmer entdecken
+        </h1>
         @foreach ($rooms as $room)
-            <x-room-list-item :model="$room" />
+            <x-room-list-item :model="$room">
+                <x-slot name="buttons">
+                    <x-link-button :href="route('room.show',['room' => $room ])">
+                        Jetzt buchen
+                    </x-link-button>
+                </x-slot>
+            </x-room-list-item>
         @endforeach
         <div class="mt-4">
             {!! $rooms->render() !!}
